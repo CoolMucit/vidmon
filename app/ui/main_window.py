a5e546391bb8f5bce9217b93c329573d445484e8
Osman Ayan <cool.mucit@gmail.com>
2025-06-28 13:22:59 +0300

    klasör yapısı standart duruma getirildi

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -1,12 +1,12 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
-from app.widgets.media_panel import MediaPanel
+from app.ui.widgets.media_panel_widget import MediaPanel
 import os
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("app/gui/main_window.ui", self)
+        uic.loadUi("app/ui/interfaces/main_window_interface.ui", self)
         self.showMaximized() 
 
         # medya paneli ayarları
@@ -14,8 +14,8 @@ class MainWindow(QMainWindow):
             media_panel_widget=self.mediaPanel
         )
 
-        # qss style dosyalarını yükle
-        style_path = os.path.join("app", "styles", "main_window.qss")
+        # STYLE qss
+        style_path = os.path.join("app", "assets", "styles", "main_window.qss")
         with open(style_path, "r") as file:
             style = file.read()
             self.setStyleSheet(style)
